0622207a7bc9e1e2207e0abc8d82a6fe238ed33d
LAPTOP-28DR15R7\frontier-Python <mat_ko327@yahoo.co.jp>
2024-03-27 11:36:56 +0900

    kentei

---
+++ kentei/11-7.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.random() * 2 - 1) # -1から1までの乱数を生成*は
+# 1以上2未満
+print(random.random() + 1) # 1から2までの乱数を生成
---
+++ kentei/low/9.py
@@ -0,0 +1,13 @@
+words = ['pine', 'apple', 'pen']
+for word in words:
+    print(word)
+    print(words)
+
+"""
+pine
+['pine', 'apple', 'pen']
+apple
+['pine', 'apple', 'pen']
+pen
+['pine', 'apple', 'pen']
+"""
